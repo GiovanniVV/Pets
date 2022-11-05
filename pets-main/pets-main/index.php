83c5101d3c401185e544e83b3ea0e612a3d4dd77
GiovanniVV <105824571+GiovanniVV@users.noreply.github.com>
2022-11-05 11:46:23 -0500

    Add files via upload
    cardogs

---
+++ pets-main/pets-main/index.php
@@ -0,0 +1,70 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Happy Pets</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+	<link rel="icon" href="images/logo.png" sizes="32x32" />
+    <link rel="icon" href="images/logo.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="images/logo.png" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+</head>
+<body>
+<div class="contenedor">
+	<header class="cabeceraPrincipal">
+		<section class="logotipo"><img src="images/logotipoPet.png"></section>
+		<nav class="menuPrincipal">
+			<a href="#">Inicio</a>
+			<a href="#">Servicios</a>
+			<a href="#">Productos</a>
+			<a href="#">Adopción</a>
+			<a href="#">Contactos</a>
+		</nav>
+	</header>
+	<section class="sliderPrincipal">
+		<img src="images/fondonara.png">
+	</section>
+	<section class="titulo">
+		<h2>Adopción</h2>
+	</section>
+	<section class="adopcion1">
+		<img src="images/Billy.png">
+		<section class="cardog">
+			<h3>Billy</h3>
+			<p>Busca una familia cariñosa, muy saludable, tiene 2 años de edad,
+			muy jugueton en especial con los niños, es aseado y se encuentra esterilizado</p>
+			<button>Más información</button>
+		</section>
+	</section>
+
+	<section class="adopcion2">
+		<img src="images/pugberto.png">
+		<section class="cardog">
+			<h3>Pugberto</h3>
+			<p>El licenciado pugberto busca una familia cariñosa que lo cuide y lo ayude a culminar
+			sus estudios, es muy cariñoso, aunque aveces se ponga un poco gruñon tiene un corazón de azucar</p>
+			<button>Más información</button>
+		</section>
+	</section>
+	<section class="doctor">
+		<h3>Doctores</h3>
+	</section>
+	<section class="sponsor">
+		<h3>Sponsor</h3>
+	</section>
+	<footer class="piePagina">
+		<section class="derechos">
+			<h6>Derechos reservados UTPL 2022</h6>
+		</section>
+		<nav class="redesSociales">
+			<a href=""><i class="fa-brands fa-facebook"></i></a>
+			<a href=""><i class="fa-brands fa-instagram"></i></a>
+			<a href=""><i class="fa-brands fa-tiktok"></i></a>
+		</nav>
+	</footer>
+</div>
+</body>
+</html>
